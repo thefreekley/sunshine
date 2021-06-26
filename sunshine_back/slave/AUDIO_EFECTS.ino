070570cd0ce73df540ba1c1a455007ed96497457
Andrey Matviichuk <thefreekley@gmail.com>
2021-06-26 23:46:40 +0300

    fix audio slider

--- sunshine_back/slave/AUDIO_EFECTS.ino
+++ sunshine_back/slave/AUDIO_EFECTS.ino
@@ -66,121 +66,6 @@ void three_color_symmetric_top_beta(int count) {       //-SET ALL LEDS TO ONE CO
 }
 
 
-int glitterPosition[10]= {LED_COUNT-1,LED_COUNT-1,LED_COUNT-1,LED_COUNT-1,LED_COUNT-1,LED_COUNT-1,LED_COUNT-1,LED_COUNT-1,LED_COUNT-1,LED_COUNT-1};
-
-void gradientMusicMode(int count , byte modeOfMusic) {       //-SET ALL LEDS TO ONE COLOR // 1 up line 2 down line 3 from center 4 to center 
-  static int last_count;
-  static int last_delta_count;
-  static int new_count = 0;
-  static unsigned  long millis_ocst = 0;
-  static unsigned int gradientIndex=0;
-
-  static byte num_of_glitter = 0;
-  
-  count = map(count, 3, 255, 0, LED_COUNT);
-  if(count>1)count = count - count%2; 
-
-   gradientIndex = rangeGrandient(gradientIndex, 1);
- 
-  if(last_count!=count){
-    last_delta_count = last_count;
-    new_count = last_delta_count;
-    
-  }
-  
-  
-  
-      if(last_count==count) {
-      if(last_delta_count > count){
-        if(new_count>count){
-          if(new_count<2)new_count = 0;
-          else new_count-=2; 
-        }
-      }
-      else if(last_delta_count < count){
-         if(new_count<count){
-          if(new_count>254)new_count = 255; //animation arrow 
-          else new_count+=2;
-            
-         }
-      }
-      if(new_count == count && count!=0 ){
-         if(last_delta_count < count)new_count = random(last_delta_count/2,count);
-         else if(last_delta_count > count)new_count = random(count/2 ,last_delta_count); // when count not enough change
-      }
-      
-      }
-
-   if(count>last_count){
-    glitterPosition[num_of_glitter] = new_count;
-    num_of_glitter++;
-    num_of_glitter%=10; 
-  }
-      
-  for(int i=0;i<10;i++){
-    if (glitterPosition[i]<LED_COUNT)glitterPosition[i]= glitterPosition[i]+1;
-  }
-
-  
-    
-   
-   if(modeOfMusic==1){
-      for (int i = 0 ; i < LED_COUNT; i++ ) {
-      if (i > LED_COUNT - new_count && modeOfMusic==2) leds[i]= ColorFromPalette( GradientPalette, rangeGrandient(gradientIndex,i)*2 );
-      else leds[i].setRGB(0,0,0);
-      }
-    }
-   else if(modeOfMusic==2){
-    for (int i = 0 ; i < LED_COUNT; i++ ) {
-    if (i < new_count) leds[i]= ColorFromPalette( GradientPalette, rangeGrandient(gradientIndex,i)*2 );
-    else leds[i].setRGB(0,0,0);
-    }
-  }
-  else if(modeOfMusic==3){
-    for (int i = 0 ; i < LED_COUNT; i++ ) {
-    if(i< LED_COUNT/2 + new_count/2 && i> LED_COUNT/2 - new_count/2) leds[i] = ColorFromPalette( GradientPalette, rangeGrandient(gradientIndex,i% (LED_COUNT/2))*2 );
-    else leds[i].setRGB(0,0,0);
-    }
-    for(int i=0;i<10;i++){
-      leds[LED_COUNT/2 + glitterPosition[i]/2-1] = ColorFromPalette( GradientPalette, rangeGrandient(gradientIndex,1)*2 );
-      leds[LED_COUNT/2 + glitterPosition[i]/2 -2].setRGB(0,0,0);
-
-      leds[LED_COUNT/2 - glitterPosition[i]/2] = ColorFromPalette( GradientPalette, rangeGrandient(gradientIndex,1)*2 );
-      leds[LED_COUNT/2 - glitterPosition[i]/2 +1].setRGB(0,0,0);
-
-    }
-    leds[LED_COUNT-1].setRGB(0,0,0);
-    leds[0].setRGB(0,0,0);
-  }
-  else if(modeOfMusic==4){
-    for (int i = 0 ; i < LED_COUNT; i++ ) {
-    if (i< new_count/2 || i> LED_COUNT - new_count/2) leds[i]= ColorFromPalette( GradientPalette, rangeGrandient(gradientIndex,i)*2 );
-    else leds[i].setRGB(0,0,0);
-  }
-  }
-  
-    
-   LEDS.show();
-   last_count = count;     
-  
-}
-
-int rangeGrandient(int index,int addPart){
-  static int riseTroggle = false;
-  if(riseTroggle){
-    if(index <120)return index+addPart;
-    else{ return 0; riseTroggle=false; }
-  }
-  else{
-    if(index >0)return index-addPart;
-    else{ return 120; riseTroggle=true; }
-  }
-}
-
-
-
-
-
 
 byte stateSplitPart[7][3] = {{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0}};
 
@@ -199,14 +84,14 @@ void frequencySplit(byte count){
     for(byte i=0; i<7; i++){
       if(bitRead(count,i)){
         if(random(0, 2)){
-        stateSplitPart[i][0]=gradientColors[1] - gradientColors[1]%speedOfFrequancy;
-        stateSplitPart[i][1]=gradientColors[2] - gradientColors[2]%speedOfFrequancy;
-        stateSplitPart[i][2]=gradientColors[3] - gradientColors[3]%speedOfFrequancy;
+        stateSplitPart[6-i][0]=gradientColors[1] - gradientColors[1]%speedOfFrequancy;
+        stateSplitPart[6-i][1]=gradientColors[2] - gradientColors[2]%speedOfFrequancy;
+        stateSplitPart[6-i][2]=gradientColors[3] - gradientColors[3]%speedOfFrequancy;
         }
         else{
-        stateSplitPart[i][0]=gradientColors[5] - gradientColors[5]%speedOfFrequancy;
-        stateSplitPart[i][1]=gradientColors[6] - gradientColors[6]%speedOfFrequancy;
-        stateSplitPart[i][2]=gradientColors[7] - gradientColors[7]%speedOfFrequancy;
+        stateSplitPart[6-i][0]=gradientColors[5] - gradientColors[5]%speedOfFrequancy;
+        stateSplitPart[6-i][1]=gradientColors[6] - gradientColors[6]%speedOfFrequancy;
+        stateSplitPart[6-i][2]=gradientColors[7] - gradientColors[7]%speedOfFrequancy;
         }
       }
       
--- sunshine_back/slave/NEW_AUDIO_EFFECTS.ino
+++ sunshine_back/slave/NEW_AUDIO_EFFECTS.ino
@@ -1,1 +1,127 @@
 
+int glitterPosition[10]= {LED_COUNT-1,LED_COUNT-1,LED_COUNT-1,LED_COUNT-1,LED_COUNT-1,LED_COUNT-1,LED_COUNT-1,LED_COUNT-1,LED_COUNT-1,LED_COUNT-1};
+
+void gradientMusicMode(int count,byte modeOfMusic){
+  static int aceleration_up = 1;
+  static int aceleration_down = 1;
+  static byte gradient_index = 0;
+  static byte alarm_for_change = 0;
+  static int current_count = 0;
+  static int go_to_count = 0;
+  static byte num_of_glitter = 0;
+  boolean add_glitter = false;
+  
+  count = map(count, 2, 255, 0, LED_COUNT);
+   if(count - go_to_count>3) add_glitter=true; 
+  if(go_to_count!=count && go_to_count==current_count) go_to_count =  count;
+  if(go_to_count==count && go_to_count==current_count && count!=0) go_to_count = random( (go_to_count - go_to_count/6) >0 ? (go_to_count - go_to_count/6): 0, (go_to_count + go_to_count/6) < LED_COUNT ? (go_to_count + go_to_count/6): LED_COUNT-1 );
+  if(go_to_count<count)go_to_count=count;
+  
+ 
+    
+  
+
+  alarm_for_change+=1;
+  alarm_for_change%=6;
+
+  
+  gradient_index++;
+  gradient_index%240;
+
+  
+  
+  if(go_to_count<current_count){
+    if(alarm_for_change==5)aceleration_down+=1;
+    aceleration_up = 1;
+    current_count-= aceleration_down;
+    if(current_count<0)current_count = 0;
+  }
+  else if(go_to_count>current_count){
+    if(alarm_for_change==5)aceleration_up+=1;
+    aceleration_down = 1;
+    current_count+=aceleration_up;
+    if(current_count>go_to_count)current_count = go_to_count;
+  }
+
+      
+   if(add_glitter){
+    glitterPosition[num_of_glitter] = current_count;
+    num_of_glitter++;
+    num_of_glitter%=10; 
+  }
+      
+  for(int i=0;i<10;i++){
+    if (glitterPosition[i]<LED_COUNT)glitterPosition[i]= glitterPosition[i]+1;
+  }
+  
+  
+  
+
+     
+   if(modeOfMusic==1){
+
+     for (int i = 0 ; i < LED_COUNT; i++ ) {
+    if (i < current_count) leds[i]= ColorFromPalette( GradientPalette, (gradient_index + i)%240 );
+    else leds[i].setRGB(0,0,0);
+    }
+      for(int i=0;i<10;i++){
+      leds[glitterPosition[i]-1] = ColorFromPalette( GradientPalette, (gradient_index + i*4)%240);
+      leds[glitterPosition[i]-2].setRGB(0,0,0);
+    }
+    leds[LED_COUNT-1].setRGB(0,0,0);
+    
+    }
+   else if(modeOfMusic==2){
+    
+    for (int i = 0 ; i < LED_COUNT; i++ ) {
+      if (i > LED_COUNT - current_count) leds[i]= ColorFromPalette( GradientPalette, (gradient_index + i)%240 );
+      else leds[i].setRGB(0,0,0);
+      }
+     
+     for(int i=0;i<10;i++){
+      leds[LED_COUNT - glitterPosition[i]] = ColorFromPalette( GradientPalette, (gradient_index + i*4)%240 );
+      leds[ (LED_COUNT - glitterPosition[i]+1)%LED_COUNT].setRGB(0,0,0);
+    }
+    leds[0].setRGB(0,0,0);
+    
+  }
+  else if(modeOfMusic==3){
+    for (int i = LED_COUNT/2; i < LED_COUNT; i++ ) {
+    if(i< LED_COUNT/2 + current_count/2) leds[i] = ColorFromPalette( GradientPalette, (gradient_index + i*6)%240 );
+    else leds[i].setRGB(0,0,0);
+    }
+    for(int i=0;i<10;i++){
+      leds[LED_COUNT/2 + glitterPosition[i]/2-1] = ColorFromPalette( GradientPalette, (gradient_index + i*4)%240 );
+      leds[LED_COUNT/2 + glitterPosition[i]/2 -2].setRGB(0,0,0);
+    }
+    leds[LED_COUNT-1].setRGB(0,0,0);
+    for(int i=0; i<LED_COUNT/2; i++){
+      leds[i] = leds[LED_COUNT-i] ;
+    }
+    leds[0].setRGB(0,0,0);
+   
+  }
+  else if(modeOfMusic==4){
+    
+  for (int i = LED_COUNT/2; i < LED_COUNT; i++ ) {
+    if(i> LED_COUNT - current_count/2) leds[i] = ColorFromPalette( GradientPalette, (gradient_index + i*2)%240 );
+    else leds[i].setRGB(0,0,0);
+    }
+
+   for(int i=0;i<10;i++){
+      leds[LED_COUNT - glitterPosition[i]/2-1] = ColorFromPalette( GradientPalette, (gradient_index + i*4)%240 );
+      leds[LED_COUNT - glitterPosition[i]/2-2].setRGB(0,0,0);
+    }
+
+    for(int i=0; i<LED_COUNT/2; i++){
+      leds[i] = leds[LED_COUNT-i] ;
+    }
+    
+   leds[LED_COUNT/2].setRGB(0,0,0);
+    
+  }
+  
+    
+   LEDS.show();
+    
+}
